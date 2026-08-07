cdc8e70fcf856390cc94e5561fe363733d402a4f
m9henson <m9henson@gmail.com>
2026-08-06 22:31:40 -0500

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.OSTL_SUPABASE = {
-  url: "https://tqitfnmaoagabaugngnx.supabase.co/rest/v1/",
+  url: "https://tqitfnmaoagabaugngnx.supabase.co",
   anonKey: "sb_publishable_YzxE5jAIbra15_I1G95YRg_11O8Qxkr" 
 };
