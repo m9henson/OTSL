1bfef35ff7a894b259d95d39bb599bb666a89308
m9henson <m9henson@gmail.com>
2026-08-06 22:39:40 -0500

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.OSTL_SUPABASE = {
   url: "https://tqitfnmaoagabaugngnx.supabase.co",
-  anonKey: "sb_publishable_YzxE5jAIbra15_I1G95YRg_11O8Qxkr" 
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxaXRmbm1hb2FnYWJhdWduZ254Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYwNTczNDEsImV4cCI6MjEwMTYzMzM0MX0.fb7WHTak57R4auNoTMZtEqQlivTW_IhcKkS2gWTdSe4" 
 };
